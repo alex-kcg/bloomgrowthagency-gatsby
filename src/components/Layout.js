3e2e544c8a2cc1a90e61e58280ad70f070a3f250
Alex Knoll <alex@kaloscreativegroup.com>
2022-07-19 08:21:18 -0600

    add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,8 +17,19 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:url" content="/" />
+        <meta property="og:url" content={`${withPrefix("/")}`} />
         <meta property="og:image" content={`${withPrefix("/")}img/og-image.jpg`} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#071014" />
+        <meta name="apple-mobile-web-app-title" content="Bloom Growth" />
+        <meta name="application-name" content="Bloom Growth" />
+        <meta name="msapplication-TileColor" content="#071014" />
+        <meta name="theme-color" content="#d8ff34" />
       </Helmet>
       <Navbar />
       <div className="flex-1 flex flex-col justify-center items-center overflow-hidden">{children}</div>
