78b7a5d07e1dc951ac71613d91ebd76e96a4c9c6
Alex Knoll <alex@kaloscreativegroup.com>
2022-08-11 17:35:37 +0200

    fix spacing

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,7 +6,7 @@ const Accordion = ({ accordionItem }) => {
   const [expanded, setExpanded] = useState(false)
 
   return (
-    <li className="border-b border-slate overflow-hidden mb-8 md:mb-16">
+    <li className="border-b border-slate overflow-hidden mb-2 md:mb-4">
       <motion.h3
         initial={false}
         onClick={() => setExpanded(expanded ? false : true)}
