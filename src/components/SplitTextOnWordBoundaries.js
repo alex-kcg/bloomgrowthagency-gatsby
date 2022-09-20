3980268ac81b919120b23b643394630597815441
Alex Knoll <alex@kaloscreativegroup.com>
2022-09-20 10:03:45 -0400

    fix for line height

--- src/components/SplitTextOnWordBoundaries.js
+++ src/components/SplitTextOnWordBoundaries.js
@@ -4,11 +4,11 @@ import PropTypes from "prop-types";
 const SplitTextOnWordBoundaries = ({ className, text, delay = 0 }) => {
   return (
     <>
-      <span className={`animate-words ${className ? className : ''}`}>
+      <span className={`animate-words leading-none ${className ? className : ''}`}>
         {text.split(' ').map((text, index) => (
           <React.Fragment key={index}>
-            <span className="animate-word-wrapper inline-block overflow-hidden transform-gpu origin-top-left">
-              <span className="animate-word inline-block transition-all ease-out duration-500 transform">
+            <span className="animate-word-wrapper inline-block align-top overflow-hidden pb-[0.1em] -mb-[0.1em]">
+              <span className="animate-word inline-block transition-all ease-out duration-500 transform-gpu">
                 {text.match(/\w+|\s+|[^\s\w]+/g).map((text, index) => (<span key={index}>{text}</span>))}
               </span>
             </span>
