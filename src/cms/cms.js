28190b590655765868d051fe15e55c04385ad1e6
Alex Knoll <alex@kaloscreativegroup.com>
2022-10-06 10:05:11 -0400

    add legal page preview template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,10 +3,12 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import LegalPagePreview from './preview-templates/LegalPagePreview'
 import SettingsPreview from './preview-templates/SettingsPreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('legal', LegalPagePreview)
 CMS.registerPreviewTemplate('settings', SettingsPreview)
